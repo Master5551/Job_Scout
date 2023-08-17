9cd8ff202f8cbf8a24cd4d0be01e841f7bd2e9f8
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2023-08-17 22:57:30 +0530

    button

--- lib/Authentication/home_page.dart
+++ lib/Authentication/home_page.dart
@@ -4,17 +4,34 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Image.asset(
-            'assets/images/startpage.png', // Replace with your SVG asset path
-            fit: BoxFit.cover, // Adjust the fit as needed
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset(
+                'assets/images/startpage.png', // Replace with your image asset path
+                fit: BoxFit.cover, // Adjust the fit as needed
+              ),
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 20.0,
+            right: 20.0,
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle the "Next" button press
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), // Adjust padding as needed
+              ),
+              child: Text('Next'),
+            ),
+          ),
+        ],
       ),
     );
   }
