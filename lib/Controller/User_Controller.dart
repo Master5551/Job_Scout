21a6185be235614234c3d7d82ce4f60c5f56df51
Master5551 <flutterproject27@gmail.com>
2023-08-31 00:02:25 +0530

    controller Getx

---
+++ lib/Controller/User_Controller.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class UserController extends GetxController {
+  final db = FirebaseFirestore.instance;
+
+  void addNote() async {}
+}
