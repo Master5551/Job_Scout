a183c5cea542dc7341dab2d3cc0912de38ccf19e
Master5551 <flutterproject27@gmail.com>
2023-09-13 00:07:53 +0530

    sending_completed

--- lib/Controller/send.dart
+++ lib/Controller/send.dart
@@ -1,41 +1,56 @@
-// send_data.dart
+
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:job_scout/Authentication/verified_page.dart'; // Import Material package
+import 'package:job_scout/Authentication/verified_page.dart';
 
 class DataSender {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> insertUserDataAndNavigate(BuildContext context) async { // Add context as a parameter
-    User? user = _auth.currentUser;
+  Future<void> sendUserDataAndNavigate(
+    BuildContext context, // Pass the context
+    String firstName,
+    String lastName,
+    String mobileNumber,
+    String profession,
+    String dob,
+    String title,
+    String about,
+  ) async {
+    try {
+      User? user = _auth.currentUser;
 
-    if (user != null) {
-      try {
+      if (user != null) {
         // Define the data you want to insert
         Map<String, dynamic> userData = {
           'userId': user.uid,
           'email': user.email,
+          'firstName': firstName,
+          'lastName': lastName,
+          'mobileNumber': mobileNumber,
+          'profession': profession,
+          'dob': dob,
+          'title': title,
+          'about': about,
           // Add more fields as needed
         };
 
         // Use the user's UID as the document ID
         await _firestore.collection('Users').doc(user.uid).set(userData);
 
-        // Insertion successful, you can now navigate to another page
-        // Example:
+        // Insertion successful, navigate to another page
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
             builder: (context) => VerifiedPage(),
           ),
         );
-      } catch (e) {
-        throw Exception('Error inserting user data: $e');
+      } else {
+        throw Exception('User is not signed in.');
       }
-    } else {
-      throw Exception('User is not signed in.');
+    } catch (e) {
+      throw Exception('Error sending user data: $e');
     }
   }
 }
--- lib/Home/profile_page.dart
+++ lib/Home/profile_page.dart
@@ -1,323 +1,328 @@
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:job_scout/Authentication/verified_page.dart';
-import 'package:job_scout/Controller/send.dart';
-import 'package:job_scout/components/my_button.dart';
+  import 'package:flutter/material.dart';
+  import 'package:image_picker/image_picker.dart';
+  import 'package:job_scout/Authentication/verified_page.dart';
+  import 'package:job_scout/Controller/send.dart';
+  import 'package:job_scout/components/my_button.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  class ProfilePage extends StatefulWidget {
+    const ProfilePage({super.key});
 
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
+    @override
+    State<ProfilePage> createState() => _ProfilePageState();
+  }
 
-class _ProfilePageState extends State<ProfilePage> {
-  late PickedFile? imageFile;
-  final ImagePicker imagePicker = ImagePicker();
-  final _first_name = TextEditingController();
-  final _last_name = TextEditingController();
-  final _mobileno = TextEditingController();
-  final _profession = TextEditingController();
-  final _dob = TextEditingController();
-  final _title = TextEditingController();
-  final _about = TextEditingController();
+  class _ProfilePageState extends State<ProfilePage> {
+    late PickedFile? imageFile;
+    final ImagePicker imagePicker = ImagePicker();
+    final _first_name = TextEditingController();
+    final _last_name = TextEditingController();
+    final _mobileno = TextEditingController();
+    final _profession = TextEditingController();
+    final _dob = TextEditingController();
+    final _title = TextEditingController();
+    final _about = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: ListView(
-          children: <Widget>[
-            imageProfile(),
-            const SizedBox(height: 20),
-            firstnameTextField(),
-            const SizedBox(height: 20),
-            lastnameTextField(),
-            const SizedBox(height: 20),
-            mobilenoTextField(),
-            const SizedBox(height: 20),
-            professionTextField(),
-            const SizedBox(height: 20),
-            dobField(),
-            const SizedBox(height: 20),
-            titleTextField(),
-            const SizedBox(height: 20),
-            aboutTextField(),
-            const SizedBox(height: 20),
-            Button(),
-            const SizedBox(height: 20),
-          ],
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile Page'),
         ),
-      ),
-    );
-  }
-
-  Widget imageProfile() {
-    return Center(
-      child: Stack(
-        children: <Widget>[
-          const CircleAvatar(
-            radius: 80.0,
-            // backgroundImage: imageFile == null
-            //     ? Image.asset("assets/images/profile.png")
-            //     : FileImage(File(imageFile!.path)),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: ListView(
+            children: <Widget>[
+              imageProfile(),
+              const SizedBox(height: 20),
+              firstnameTextField(),
+              const SizedBox(height: 20),
+              lastnameTextField(),
+              const SizedBox(height: 20),
+              mobilenoTextField(),
+              const SizedBox(height: 20),
+              professionTextField(),
+              const SizedBox(height: 20),
+              dobField(),
+              const SizedBox(height: 20),
+              titleTextField(),
+              const SizedBox(height: 20),
+              aboutTextField(),
+              const SizedBox(height: 20),
+              Button(),
+              const SizedBox(height: 20),
+            ],
           ),
-          Positioned(
-            bottom: 20.0,
-            right: 20.0,
-            child: InkWell(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: ((builder) => bottomSheet()),
-                );
-              },
-              child: Icon(
-                Icons.camera_alt,
-                color: Colors.teal[200],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget bottomSheet() {
-    return Container(
-      height: 100.0,
-      width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 20,
-      ),
-      child: Column(children: <Widget>[
-        const Text(
-          "Choose Profile photo",
-          style: TextStyle(
-            fontSize: 20.0,
-          ),
-        ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton.icon(
-              icon: const Icon(Icons.camera),
-              onPressed: () {
-                takePhoto(ImageSource.camera);
-              },
-              label: const Text("Camera"),
-            ),
-            TextButton.icon(
-              icon: const Icon(Icons.image),
-              onPressed: () {
-                takePhoto(ImageSource.gallery);
-              },
-              label: const Text("Gallery"),
-            ),
-          ],
-        )
-      ]),
-    );
-  }
-
-  void takePhoto(ImageSource source) async {
-    // final pickedFile = await imagePicker.getImage(source: ImageSource.camera);
-    setState(() {
-      // imageFile = pickedFile;
-    });
-  }
-
-  Widget firstnameTextField() {
-    return TextFormField(
-      controller: _first_name,
-      decoration: InputDecoration(
-          border: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.green[300],
-          ),
-          labelText: "first name",
-          helperText: "Name Can't be empty",
-          hintText: "John Doe"),
-    );
-  }
-
-  Widget lastnameTextField() {
-    return TextFormField(
-      controller: _last_name,
-      decoration: InputDecoration(
-          border: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.green[300],
-          ),
-          labelText: "last name",
-          helperText: "Name Can't be empty",
-          hintText: "John Doe"),
-    );
-  }
-
-  Widget mobilenoTextField() {
-    return TextFormField(
-      controller: _mobileno,
-      decoration: InputDecoration(
-          border: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.green[300],
-          ),
-          labelText: "Enter Your Mobile Number",
-          helperText: "Name Can't be empty",
-          hintText: "John Doe"),
-    );
-  }
-
-  Widget professionTextField() {
-    return TextFormField(
-      controller: _profession,
-      decoration: InputDecoration(
-          border: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          prefixIcon: Icon(
-            Icons.person,
-            color: Colors.green[300],
-          ),
-          labelText: "Profession",
-          helperText: "Profession Can't be empty",
-          hintText: "GoLang Developer"),
-    );
-  }
-
-  Widget dobField() {
-    return TextFormField(
-      controller: _dob,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.teal,
-        )),
-        focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.orange,
-          width: 2,
-        )),
-        prefixIcon: Icon(
-          Icons.person,
-          color: Colors.green[300],
-        ),
-        labelText: "Date of Birth",
-        helperText: "Provide DOB on dd/mm/yyyy",
-        hintText: "19/07/2003",
-      ),
-    );
-  }
-
-  Widget titleTextField() {
-    return TextFormField(
-      controller: _title,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.teal,
-        )),
-        focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.orange,
-          width: 2,
-        )),
-        prefixIcon: Icon(
-          Icons.person,
-          color: Colors.green[300],
-        ),
-        labelText: "title",
-        helperText: "It can't be empty",
-        hintText: "Full Stack Developer",
-      ),
-    );
-  }
-
-  Widget aboutTextField() {
-    return TextFormField(
-      controller: _about,
-      maxLines: 4,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.teal,
-        )),
-        focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-          color: Colors.orange,
-          width: 2,
-        )),
-        labelText: "About",
-        helperText: "Write about yourself",
-        hintText: "Full Stack Developer",
-      ),
-    );
-  }
-
-  Widget Button() {
-    return MyButton(
-      onTap: () async {
-    try {
-       DataSender();
-      // If the data insertion was successful, you can navigate to another page here.
-      // Example:
-      // Navigator.of(context).pushReplacement(
-      //   MaterialPageRoute(
-      //     builder: (context) => VerifiedPage(),
-      //   ),
-      // );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Error: $e'),
         ),
       );
     }
-  },
-  buttonText: "Submit",
 
-      
-      
-    );
-  }
+    Widget imageProfile() {
+      return Center(
+        child: Stack(
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 80.0,
+              // backgroundImage: imageFile == null
+              //     ? Image.asset("assets/images/profile.png")
+              //     : FileImage(File(imageFile!.path)),
+            ),
+            Positioned(
+              bottom: 20.0,
+              right: 20.0,
+              child: InkWell(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: ((builder) => bottomSheet()),
+                  );
+                },
+                child: Icon(
+                  Icons.camera_alt,
+                  color: Colors.teal[200],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget bottomSheet() {
+      return Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 20,
+        ),
+        child: Column(children: <Widget>[
+          const Text(
+            "Choose Profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton.icon(
+                icon: const Icon(Icons.camera),
+                onPressed: () {
+                  takePhoto(ImageSource.camera);
+                },
+                label: const Text("Camera"),
+              ),
+              TextButton.icon(
+                icon: const Icon(Icons.image),
+                onPressed: () {
+                  takePhoto(ImageSource.gallery);
+                },
+                label: const Text("Gallery"),
+              ),
+            ],
+          )
+        ]),
+      );
+    }
+
+    void takePhoto(ImageSource source) async {
+      // final pickedFile = await imagePicker.getImage(source: ImageSource.camera);
+      setState(() {
+        // imageFile = pickedFile;
+      });
+    }
+
+    Widget firstnameTextField() {
+      return TextFormField(
+        controller: _first_name,
+        decoration: InputDecoration(
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.green[300],
+            ),
+            labelText: "first name",
+            helperText: "Name Can't be empty",
+            hintText: "John Doe"),
+      );
+    }
+
+    Widget lastnameTextField() {
+      return TextFormField(
+        controller: _last_name,
+        decoration: InputDecoration(
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.green[300],
+            ),
+            labelText: "last name",
+            helperText: "Name Can't be empty",
+            hintText: "John Doe"),
+      );
+    }
+
+    Widget mobilenoTextField() {
+      return TextFormField(
+        controller: _mobileno,
+        decoration: InputDecoration(
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.green[300],
+            ),
+            labelText: "Enter Your Mobile Number",
+            helperText: "Name Can't be empty",
+            hintText: "John Doe"),
+      );
+    }
+
+    Widget professionTextField() {
+      return TextFormField(
+        controller: _profession,
+        decoration: InputDecoration(
+            border: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.green[300],
+            ),
+            labelText: "Profession",
+            helperText: "Profession Can't be empty",
+            hintText: "GoLang Developer"),
+      );
+    }
+
+    Widget dobField() {
+      return TextFormField(
+        controller: _dob,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.teal,
+          )),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.orange,
+            width: 2,
+          )),
+          prefixIcon: Icon(
+            Icons.person,
+            color: Colors.green[300],
+          ),
+          labelText: "Date of Birth",
+          helperText: "Provide DOB on dd/mm/yyyy",
+          hintText: "19/07/2003",
+        ),
+      );
+    }
+
+    Widget titleTextField() {
+      return TextFormField(
+        controller: _title,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.teal,
+          )),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.orange,
+            width: 2,
+          )),
+          prefixIcon: Icon(
+            Icons.person,
+            color: Colors.green[300],
+          ),
+          labelText: "title",
+          helperText: "It can't be empty",
+          hintText: "Full Stack Developer",
+        ),
+      );
+    }
+
+    Widget aboutTextField() {
+      return TextFormField(
+        controller: _about,
+        maxLines: 4,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.teal,
+          )),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            color: Colors.orange,
+            width: 2,
+          )),
+          labelText: "About",
+          helperText: "Write about yourself",
+          hintText: "Full Stack Developer",
+        ),
+      );
+    }
+
+   Widget Button() {
+  return MyButton(
+    onTap: () async {
+      try {
+        String firstName = _first_name.text;
+        String lastName = _last_name.text;
+        String mobileNumber = _mobileno.text;
+        String profession = _profession.text;
+        String dob = _dob.text;
+        String title = _title.text;
+        String about = _about.text;
+
+        // Create an instance of DataSender and call sendUserDataAndNavigate
+        DataSender dataSender = DataSender();
+        await dataSender.sendUserDataAndNavigate(
+          context,
+          firstName,
+          lastName,
+          mobileNumber,
+          profession,
+          dob,
+          title,
+          about,
+        );
+      } catch (e) {
+        print('Error: $e');
+      }
+    },
+    buttonText: "Submit",
+  );
 }
+  }
